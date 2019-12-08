4992f39895629713a476beb62aaec11a9592c4d7
Emily Richardson <49946051+richar35@users.noreply.github.com>
2019-12-08 18:31:39 -0500

    Add files via upload

---
+++ VacationPlanner/VacationPlanner/Expense.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationPlanner
+{
+    public class Expense
+    {
+
+
+        #region FIELDS
+
+        
+        private double _cost;
+        private string _name;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string Name
+        {
+           get { return _name; }
+           set { _name = value; }
+        }
+
+       
+
+        public double Cost
+        {
+            get { return _cost; }
+            set { _cost = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Expense()
+        {
+
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+    }
+}
